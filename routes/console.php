3c2492e7e3c41ede3920c2f0e586176845974763
dava-alt-f4 <davagalangsamudra06@gmail.com>
2026-09-10 10:23:49 +0700

    feat: add end of month auto-sweep task

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Models\User;
+use App\Services\FinanceService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function (FinanceService $financeService) {
+    $users = User::all();
+
+    foreach ($users as $user) {
+        $financeService->processAutoSweep($user);
+    }
+})->lastDayOfMonth('23:59')->name('harvey:auto-sweep');
